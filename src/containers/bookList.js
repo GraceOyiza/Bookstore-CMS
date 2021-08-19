d956702512164caec7a0a181c08963f35fae0331
GraceOyiza <graceoyiza13@gmail.com>
2021-08-19 11:18:51 +0100

    Fetch the book from state using connect

--- src/containers/bookList.js
+++ src/containers/bookList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
-import { generateRandomNumber } from '../common';
+import { generateRandomNumber } from '../table';
 
 function BooksList({ books }) {
   const allBooks = books.map(book => <Book book={book} key={generateRandomNumber()} />);
