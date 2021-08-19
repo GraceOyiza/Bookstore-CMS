9da0e129e04104c4096e840ced660a65b1910955
GraceOyiza <graceoyiza13@gmail.com>
2021-08-19 10:26:17 +0100

    Generate random number and add default books

---
+++ src/table.js
@@ -0,0 +1,36 @@
+export const generateRandomNumber = (range = 10000) => Math.floor(Math.random() * range);
+
+export const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
+export const defaultBooks = [
+  {
+    id: generateRandomNumber(),
+    title: 'A GAME OF THRONES',
+    author: 'George R. R. Martin',
+    category: categories[generateRandomNumber(categories.length)],
+  },
+  {
+    id: generateRandomNumber(),
+    title: 'BORN A CRIME',
+    author: 'Trevor Noah',
+    category: categories[generateRandomNumber(categories.length)],
+  },
+  {
+    id: generateRandomNumber(),
+    title: 'AN ARMY AT DAWN',
+    author: 'Rick Atkinson',
+    category: categories[generateRandomNumber(categories.length)],
+  },
+  {
+    id: generateRandomNumber(),
+    title: 'THE HUNTING OF HILL HOUSE',
+    author: 'Shirley Jackson',
+    category: categories[generateRandomNumber(categories.length)],
+  },
+  {
+    id: generateRandomNumber(),
+    title: 'THE SNOWY DATE',
+    author: 'Ezra Jack',
+    category: categories[generateRandomNumber(categories.length)],
+  },
+];
